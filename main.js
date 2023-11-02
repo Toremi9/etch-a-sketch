e48a1a03f9bd74fa70cd0daf932e5cd6cac1638b
Toremi9 <142730474+Toremi9@users.noreply.github.com>
2023-11-02 11:20:14 +0100

    finished etch - a - sketch

--- main.js
+++ main.js
@@ -11,9 +11,12 @@ const headline = document.createElement('h1');
 headline.textContent = 'Etch-a-sketch';
 headlinediv.appendChild(headline);
 
+const gridAndMenu = document.createElement('div');
+gridAndMenu.className = 'gridAndMenu';
+document.body.appendChild(gridAndMenu);
+
 let grid = document.createElement('div');
 grid.className = 'grid';
-grid.setAttribute('style', 'display: flex; border: solid 2px; justify-content: center; margin: auto; width: 500px; height: 500px;');
 
 for (let i = 0; i < rows; i++){
     const row = document.createElement('div');
@@ -22,18 +25,75 @@ for (let i = 0; i < rows; i++){
         cell.className = 'cell';
         cell.setAttribute('style', `width: ${500 / rows}px; height: ${500 / rows}px; border: solid black 1px; box-sizing: border-box;`);
         cell.addEventListener('mouseover', () => {
-            cell.setAttribute('style', `width: ${500 / rows}px; height: ${500 / rows}px; border: solid black 1px; background-color: yellow; box-sizing: border-box;`);
+            cell.setAttribute('style', `width: ${500 / rows}px; height: ${500 / rows}px; border: solid black 1px; background-color: black; box-sizing: border-box;`);
         });
         row.appendChild(cell);
     }
     grid.appendChild(row);
 }
-main.appendChild(grid);
 
-const underGrid = document.createElement('div');
-underGrid.className = 'underGrid';
-main.appendChild(underGrid);
 
-const leftmid = document.createElement('div');
-leftmid.textContent = "Choose a color";
-underGrid.appendChild(leftmid);
+const leftOfGrid = document.createElement('div');
+leftOfGrid.setAttribute('style','display: flex; justify-content: center; align-content: center;');
+leftOfGrid.className = 'leftOfGrid';
+gridAndMenu.appendChild(leftOfGrid);
+
+const bottomMid = document.createElement('div');
+bottomMid.setAttribute('style', 'display: flex; flex-wrap: wrap; align-items: center;');
+leftOfGrid.appendChild(bottomMid);
+
+const chooseNumber = document.createElement('p');
+chooseNumber.textContent = "Grid size (max 100):";
+chooseNumber.setAttribute('style', ' margin: 0px;');
+bottomMid.appendChild(chooseNumber);
+
+const input = document.createElement('input');
+input.setAttribute('style', 'margin-right: 10px; margin-left: 10px;');
+bottomMid.appendChild(input);
+
+const enterBtn = document.createElement('button');
+enterBtn.textContent = "Enter";
+enterBtn.addEventListener('click', () => {
+    let rememberValue = input.value;
+    input.value = '';
+
+    if (rememberValue > 100) {
+        alert('We told you max is 100, so we changed it to 100');
+        rememberValue = 100;
+    };
+
+    if (rememberValue < 1) {
+        rememberValue = 1;
+    };
+
+    rows = rememberValue;
+    columns = rememberValue;
+
+    let newGrid = document.createElement('div');
+    newGrid.className = 'grid';
+
+    for (let i = 0; i < rows; i++){
+        const row = document.createElement('div');
+        for (let j = 0; j < columns; j++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.setAttribute('style', `width: ${500 / rows}px; height: ${500 / rows}px; border: solid black 1px; box-sizing: border-box;`);
+            cell.addEventListener('mouseover', () => {
+                cell.setAttribute('style', `width: ${500 / rows}px; height: ${500 / rows}px; border: solid black 1px; background-color: black; box-sizing: border-box;`);
+            });
+            row.appendChild(cell);
+        }
+        newGrid.appendChild(row);
+        }
+
+    
+    gridAndMenu.removeChild(grid);
+    gridAndMenu.appendChild(newGrid);
+    
+    grid = newGrid;
+
+
+});
+bottomMid.appendChild(enterBtn);
+
+gridAndMenu.appendChild(grid);
